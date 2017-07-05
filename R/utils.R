85bb76bdf58ad48703adc074c5969b84010709ab
Jean-Olivier Irisson <irisson@normalesup.org>
2017-07-05 16:21:14 +0200

    Add function to remove empty columns in data.frames

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,16 @@
 #' @importFrom stringr str_c
 #' @importFrom stats na.omit
 NULL
+
+
+#' Remove empty columns from a data.frame
+#'
+#' Useful to remove unmapped columns extracted from EcoTaxa, which are all empty
+#' @param x a data.frame
+#' @examples
+#' x <- data.frame(a=c(1,NA), b=NA, c=c(NA, "foo"))
+#' remove_empty_cols(x)
+#' @export
+remove_empty_cols <- function(x) {
+  x[, sapply(x, function(X) {!all(is.na(X))})]
+}
