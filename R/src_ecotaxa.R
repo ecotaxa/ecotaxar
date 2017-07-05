e542654d5bd3970ed1577c95907faafee3c24365
Jean-Olivier Irisson <irisson@normalesup.org>
2017-07-05 23:16:46 +0200

    Add examples

--- R/src_ecotaxa.R
+++ R/src_ecotaxa.R
@@ -1,8 +1,10 @@
 #' Connect to the database of EcoTaxa
 #'
+#' @examples
+#' db <- src_ecotaxa()
+#' db
 #' @export
 src_ecotaxa <- function() {
-  # connect to db
   db <- dplyr::src_postgres(dbname="ecotaxa", host="193.49.112.43", user="zoo", password="z004ecot@x@")
   return(db)
 }
--- R/tbl_ecotaxa.R
+++ R/tbl_ecotaxa.R
@@ -1,6 +1,12 @@
 #' Connect to EcoTaxa tables
 #'
 #' @param db a connection to the EcoTaxa database (from \code{\link{src_ecotaxa}})
+#' @examples
+#' db <- src_ecotaxa()
+#' ls()
+#' tbl_ecotaxa(db)
+#' ls()
+#' head(projects)
 #' @export
 tbl_ecotaxa <- function(db) {
   for (table in c("projects", "samples", "acquisitions", "process", "objects", "obj_head", "images", "taxonomy")) {
