576d13888717e353b894d36f8edf92e93154d313
Jean-Olivier Irisson <irisson@normalesup.org>
2017-07-05 15:41:21 +0200

    Change name of data base connection function

--- R/connect_ecotaxa.R
+++ R/connect_ecotaxa.R
@@ -1,7 +1,7 @@
 #' Connect to the database of EcoTaxa
 #'
 #' @export
-connect_ecotaxa <- function() {
+src_ecotaxa <- function() {
   # connect to db
   db <- dplyr::src_postgres(dbname="ecotaxa", host="193.49.112.43", user="zoo", password="z004ecot@x@")
   return(db)
