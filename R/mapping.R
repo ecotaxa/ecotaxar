f9304b2714d23c9bef4127fa2b92e594070d3457
Jean-Olivier Irisson <irisson@normalesup.org>
2017-07-05 23:37:24 +0200

    Simplify documentation of mapping functions

--- R/mapping.R
+++ R/mapping.R
@@ -1,27 +1,15 @@
-#' Parse a mapping specification into a named vector
+#' Parse a mapping specification
 #'
-#' @param mapping an EcoTaxa mapping string
-#' @examples
-#' mapping <- "n01=area\nn02=mean\nn03=stddev"
-#' parse_mapping(mapping)
-#' @export
-parse_mapping <- function(mapping) {
-  # reformat mapping for dplyr::rename
-  mapping <- stringr::str_split(mapping, "\n|=")[[1]]
-  mapping_names <- mapping[seq(2, length(mapping), by=2)]
-  mapping <- mapping[seq(1, length(mapping), by=2)]
-  names(mapping) <- mapping_names
-  return(mapping)
-}
-
-#' Parse a mapping specification and rename columns
-#'
+#' @rdname mapping
 #' @param x a data.frame with un-mapped columns (in the form "n01", "n02", etc.)
-#' @inheritParams parse_mapping
+#' @param mapping an EcoTaxa mapping string
 #' @examples
 #' d <- data.frame(id=c(1234, 1235), n01=c(12,34), n02=c(125,156))
 #' mapping <- "n01=area\nn02=mean\nn03=stddev"
+#'
 #' map_names(d, mapping)
+#'
+#' parse_mapping(mapping)
 #' @export
 map_names <- function(x, mapping) {
   # parse mapping
@@ -31,3 +19,15 @@ map_names <- function(x, mapping) {
   # rename columns
   dplyr::rename(x, UQS(mapping))
 }
+
+#' @export
+#' @rdname mapping
+parse_mapping <- function(mapping) {
+  # reformat mapping for dplyr::rename
+  mapping <- stringr::str_split(mapping, "\n|=")[[1]]
+  mapping_names <- mapping[seq(2, length(mapping), by=2)]
+  mapping <- mapping[seq(1, length(mapping), by=2)]
+  names(mapping) <- mapping_names
+  return(mapping)
+}
+
