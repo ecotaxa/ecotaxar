8fbb5f971c2439829228b3da6984646d7392e6f2
Jean-Olivier Irisson <irisson@normalesup.org>
2017-10-18 13:21:16 +0200

    Deal with NA input in extract_taxo()

--- R/taxo.R
+++ R/taxo.R
@@ -14,36 +14,47 @@
 #' extract_taxo(db, 84000:84003, recursive=FALSE)
 #' @export
 extract_taxo <- function(db, ids, recursive=TRUE) {
-  # reduce to unique taxa
-  ids <- unique(na.omit(ids))
-
-  if (recursive) {
-    # get full taxonomy until the root from all taxa
-    # (with some recursive SQL-fu!)
-    taxo <- dplyr::tbl(db, dplyr::sql(stringr::str_c(
-      "WITH RECURSIVE rq AS (
-        SELECT DISTINCT t.id,t.name,t.parent_id
-        FROM taxonomy t WHERE t.id IN (", stringr::str_c(ids, collapse=","), ")
-      union
-        SELECT t.id,t.name,t.parent_id
-        FROM rq JOIN taxonomy t ON rq.parent_id = t.id
-      )
-      SELECT * FROM rq")))
-    taxo <- dplyr::collect(taxo)
-  } else {
-    # get taxonomy reduced to the observed taxa
-    taxo <- dplyr::tbl(db, "taxonomy") %>%
-      dplyr::filter(id %in% ids) %>%
-      dplyr::select(-dplyr::starts_with("nbrobj")) %>%
-      dplyr::collect()
-  }
-
-  # check, a posteriori, that we have at least all the input ids
-  # NB: doing it a posteriori avoids having to pay the cost to search the taxonomy before extracting it
+  # remove NAs in classif ids
+  # = unidentified objects which do not matter to compute the taxonomy
   ids <- na.omit(ids)
-  ids_ok <- ids %in% taxo$id
-  if (!all(ids_ok)) {
-    stop("Some ids in `ids` could not be found: ", stringr::str_c(unique(ids[!ids_ok]), collapse=", "))
+
+  # reduce to unique taxa
+  ids <- unique(ids)
+  
+  if (length(ids) == 0) {
+    # if all ids are missing, do not extract anything
+    taxo <- data.frame(id=NA, parent=NA, name=NA)
+
+  } else {
+    # otherwise proceed with the extraction
+
+    if (recursive) {
+      # get full taxonomy until the root from all taxa
+      # (with some recursive SQL-fu!)
+      taxo <- dplyr::tbl(db, dplyr::sql(stringr::str_c(
+        "WITH RECURSIVE rq AS (
+          SELECT DISTINCT t.id,t.name,t.parent_id
+          FROM taxonomy t WHERE t.id IN (", stringr::str_c(ids, collapse=","), ")
+        union
+          SELECT t.id,t.name,t.parent_id
+          FROM rq JOIN taxonomy t ON rq.parent_id = t.id
+        )
+        SELECT * FROM rq")))
+      taxo <- dplyr::collect(taxo)
+    } else {
+      # get taxonomy reduced to the observed taxa
+      taxo <- dplyr::tbl(db, "taxonomy") %>%
+        dplyr::filter(id %in% ids) %>%
+        dplyr::select(-dplyr::starts_with("nbrobj")) %>%
+        dplyr::collect()
+    }
+
+    # check, a posteriori, that we have at least all the input ids
+    # NB: doing it a posteriori avoids having to pay the cost to search the taxonomy before extracting it
+    ids_ok <- ids %in% taxo$id
+    if (!all(ids_ok)) {
+      stop("Some ids in `ids` could not be found: ", stringr::str_c(unique(ids[!ids_ok]), collapse=", "))
+    }
   }
 
   # give it a special class to do stuff with
