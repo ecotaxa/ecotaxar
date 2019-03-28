e8090fd5cca83d0f200f71eca1e447d96ffa990e
Jean-Olivier Irisson <irisson@normalesup.org>
2019-03-28 23:08:06 +0100

    Add a way to cleanly disconnect from database

--- R/src_ecotaxa.R
+++ R/src_ecotaxa.R
@@ -1,10 +1,20 @@
-#' Connect to the database of EcoTaxa
+#' Database connection to EcoTaxa
+#'
+#' Connect to and disconnect from the EcoTaxa database (in read-only mode).
 #'
 #' @examples
 #' db <- src_ecotaxa()
 #' db
+#' db_disconnect(db)
+#' # NB: always disconnect after use. Leaving open connections clobbers the server
 #' @export
 src_ecotaxa <- function() {
   db <- dplyr::src_postgres(dbname="ecotaxa", host="ecotaxa.obs-vlfr.fr", user="zoo", password="z004ecot@x@")
   return(db)
+}
+
+#' @rdname src_ecotaxa
+#' @export
+db_disconnect <- function(x) {
+  RPostgreSQL::dbDisconnect(db$con)
 }
