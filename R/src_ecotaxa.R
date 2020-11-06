b18921c0c202500856e75c108c822474fb7096ed
Jean-Olivier Irisson <irisson@normalesup.org>
2020-11-06 22:38:20 +0000

    New approach to db connection
    Use RPostgreSQL directly. src_postgres is deprecated in dbplyr.

--- R/src_ecotaxa.R
+++ R/src_ecotaxa.R
@@ -2,19 +2,49 @@
 #'
 #' Connect to and disconnect from the EcoTaxa database (in read-only mode).
 #'
+#' @param x a database connection created by [db_connect_ecotaxa()]
+#'
+#' @return An object of class
+#' @export
+#'
 #' @examples
-#' db <- src_ecotaxa()
+#' db <- db_connect_ecotaxa()
 #' db
 #' db_disconnect(db)
 #' # NB: always disconnect after use. Leaving open connections clobbers the server
-#' @export
-src_ecotaxa <- function() {
-  db <- dplyr::src_postgres(dbname="ecotaxa", host="ecotaxa.obs-vlfr.fr", user="zoo", password="z004ecot@x@")
+db_connect_ecotaxa <- function() {
+  db <- RPostgreSQL::dbConnect("PostgreSQL", host="ecotaxa.obs-vlfr.fr", dbname="ecotaxa", user="zoo", password="z004ecot@x@")
   return(db)
 }
 
-#' @rdname src_ecotaxa
+# Define a print method for a connection object
+setMethod(
+  f = "show",
+  signature = "PostgreSQLConnection",
+  definition = function(object){
+    tables <- RPostgreSQL::dbListTables(object)
+    tables <- sort(tables)
+    tables <- str_c(tables, collapse=", ")
+    cat("tbls:", tables)
+  }
+)
+
+#' @rdname db_connect_ecotaxa
+#' @export
+db_disconnect_ecotaxa <- function(x) {
+  RPostgreSQL::dbDisconnect(x$con)
+}
+
+#' @rdname db_connect_ecotaxa
+#' @export
+src_ecotaxa <- function() {
+  .Deprecated("db_connect_ecotaxa")
+  db_connect_ecotaxa()
+}
+
+#' @rdname db_connect_ecotaxa
 #' @export
 db_disconnect <- function(x) {
-  RPostgreSQL::dbDisconnect(x$con)
+  .Deprecated("db_disconnect_ecotaxa")
+  db_disconnect_ecotaxa(x)
 }
