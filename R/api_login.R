4cfe1dbb243fff1565b4d8c25915874188f0fb2d
Jean-Olivier Irisson <irisson@normalesup.org>
2021-05-06 18:19:28 +0200

    Now EcoTaxa has a proper SSL certificate.

--- R/api_login.R
+++ R/api_login.R
@@ -16,7 +16,6 @@
 api_login <- function(username, password, cache=TRUE) {
   response <- httr::POST(
     url=str_c(api_url(), "login"),
-    config=httr::config(ssl_verifypeer=FALSE),
     body=list(username=username, password=password),
     encode="json"
   )
