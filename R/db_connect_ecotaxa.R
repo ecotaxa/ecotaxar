2b0aa6e716079d5f8a9a2f25befc087452ff48c8
Jean-Olivier Irisson <irisson@normalesup.org>
2022-09-07 12:26:26 +0200

    Add port for EcoPart

--- R/db_connect_ecotaxa.R
+++ R/db_connect_ecotaxa.R
@@ -44,7 +44,7 @@ db_connect_ecotaxa <- function(...) {
 #' @rdname db_connect
 #' @export
 db_connect_ecopart <- function(...) {
-  db <- db_connect(host="ecotaxa.obs-vlfr.fr", dbname="ecopart", user="zoo", password="z004ecot@x@", ...)
+  db <- db_connect(host="ecotaxa.obs-vlfr.fr", dbname="ecopart", user="zoo", password="z004ecot@x@", port=5435, ...)
   return(db)
 }
 
